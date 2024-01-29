79f05e3eb602f504de4023905cfd19c0a7e96213
sairam <sairm21@gmail.com>
2024-01-29 20:08:26 +0530

    CI.groovy file is added for code DRY

---
+++ vars/CI.groovy
@@ -0,0 +1,32 @@
+def call() {
+    node('Workstation') {
+
+             if (env.cibuild == "java") {
+                stage('Build') {
+                         sh "npm i"
+                 }
+            }
+
+
+            stage('Unit tests') {
+                     echo "unit tests"
+                    // sh "npm test"
+             }
+
+            stage('Code Analaysis') {
+                     echo "code analasys"
+                    // sh 'sonar-scanner -Dsonar.host.url=http://172.31.47.19:9000 -Dsonar.login="admin" -Dsonar.password="S@ir&m1221" -Dsonar.projectKey=cart -Dsonar.qualitygate.wait=true'
+             }
+
+            stage('Security Scans') {
+                     echo "security scans"
+             }
+
+            stage('Publish a Artifact') {
+                when {
+                    expression { env.TAG_NAME ==~ ".*" }
+                }
+                     echo "publish artifacts"
+             }
+        }
+    }
