45a829fee12a0ec0b32719ab0df0665e4187460d
sairam <sairm21@gmail.com>
2024-02-01 20:07:28 +0530

    modified CI file to get branch and tags details and to push artifacts.

--- vars/CI.groovy
+++ vars/CI.groovy
@@ -1,6 +1,18 @@
 def call() {
     node('Workstation') {
 
+        stage('code checkout'){
+
+            sh 'ls -A1 | xargs rm -rf'
+
+            if (env.TAG_NAME ==~ ".*") {
+                env.gitbranch = "refs/tags/${env.TAG_NAME}"
+            } else {
+                env.gitbranch = "${env.TAG_NAME}"
+            }
+                checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: "https://github.com/sairm21/${env.component}"]], branches: [[name: gitbranch]]],poll: false
+            }
+
              if (env.cibuild == "java") {
                 stage('Build') {
                          sh "mvn package"
@@ -30,6 +42,14 @@ def call() {
         if (env.TAG_NAME ==~ ".*"){
             stage('Publish a Artifact') {
                 echo "publish artifacts"
+
+                sh 'rm -rf Jenkinsfile'
+
+                sh 'echo ${TAG_NAME} > version'
+
+                sh 'zip-r ${component}-${TAG_NAME}.zip'
+
+                sh 'curl -v -u admin:S@ir&m1221 --upload-file ${component}-${TAG_NAME}.zip http://172.31.15.226:8081/repository/${component}-${TAG_NAME}.zip'
             }
         }
         }
